43faf5cf628a1b0e1b6983727af82db4e9ef8e29
Artem <artembabuhkin@gmail.com>
2023-04-27 20:26:28 +0300

    Выполнил задание 3_2

---
+++ Arrays_3_2/Arrays_3_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+#include <math.h>
+using namespace std;
+
+
+string bin_number; //for regex
+
+
+void set_number() {
+	while (!regex_match(bin_number, regex("([1][0-1]{0,32})|[0]"))) {
+			getline(cin, bin_number);
+		}
+}
+
+
+int main()
+{
+	set_number();
+	int dec_number=0, length;
+	int i_pow = length = size(bin_number)-1;
+	
+	for (int k = 0; k <= length; k++) {
+		string current_number{ bin_number[k] };
+		dec_number += stoi(current_number)*pow(2, i_pow);
+		i_pow--;
+	} 
+	cout << dec_number;
+
+	return 0;
+}
