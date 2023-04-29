25eb833d8d3b25b7885b9b19896824430797c3f3
Бабушкин Артём Германович <1bag@spbt.local>
2023-04-29 13:21:56 +0300

    Внёс правки

--- Arrays_1_12/Arrays_1_12.cpp
+++ Arrays_1_12/Arrays_1_12.cpp
@@ -10,7 +10,7 @@ void set_array() {
 	string tmp_array; //for stoi()
 	for (int i = 0; i <= n - 1; i++) {
 		tmp_array = ("");
-		while (!regex_match(tmp_array, regex("(([1-9][0-9]{1,16})|([1-9])|([0]))([.][0-9]{0,16}[1-9]){0,1}"))) {
+		while (!regex_match(tmp_array, regex("([-]{0,1}(([1-9][0-9]{1,16})|([1-9]))([.][0-9]{0,16}[1-9]){0,1})|[0]"))) {
 			cout << "\nEnter a array element # " << i + 1 << ": ";
 			getline(cin, tmp_array);
 			try {
@@ -32,11 +32,12 @@ void show_array() {
 void sorting_array() {
 	double tmp_m;
 		for (int k = n; k > 1; k--) {
-			for (int i = 0; i <= k - 1; i++) {
+			for (int i = 0; i <= k - 2; i++) {
 				if (m[i] < m[i + 1]) {
 					tmp_m = m[i + 1];
 					m[i + 1] = m[i];
 					m[i] = tmp_m;
+					cout << "current iteration: " << i << "." << k <<"		" << m[i] << "	" << m[i + 1] << "\n";
 				}
 			}
 		}
